673b15581cb5973a0cd2cfa0063a480d71ace60b
Frank Chen <U589736@dow.com>
2018-11-16 11:29:14 +0800

    Add media components

--- knbase/src/app/app.module.ts
+++ knbase/src/app/app.module.ts
@@ -5,19 +5,33 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Camera } from '@ionic-native/camera';
+import { MediaCapture } from '@ionic-native/media-capture';
+import { Media } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot()
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Camera,
+    MediaCapture,
+    Media,
+    File
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- knbase/src/app/home/home.page.ts
+++ knbase/src/app/home/home.page.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Answer } from '../model/answer';
+import { ToastController } from '@ionic/angular';
+
+import { MediaCapture, MediaFile, CaptureError, CaptureVideoOptions } from '@ionic-native/media-capture';
+import { Storage } from '@ionic/storage';
+import { Media, MediaObject } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
+
+const MEDIA_FILES_KEY = 'mediaFiles';
 
 @Component({
   selector: 'app-home',
@@ -12,15 +20,56 @@ export class HomePage {
   answerList: Answer[];
   answerGot: boolean;
 
+
+  mediaFiles = [];
+  @ViewChild('myvideo') myVideo: any;
+
+
+  constructor(public toastController: ToastController,
+    private mediaCapture: MediaCapture, private storage: Storage, private file: File, private media: Media) { }
+
   getAnswer() {
     if (this.question) {
-      alert('Test: ' + this.question);
+      this.presentToast('Test: ' + this.question);
       this.answerGot = true;
       if (!this.answerList) {
-        this.answerList = [{ title: 'Test', content: 'Test content'}];
+        this.answerList = [{ title: 'Test', content: 'Test content' }];
       }
     } else {
-      alert('Please input the question.');
+      this.presentToast('Please input the question.');
     }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  ionViewDidLoad() {
+    this.storage.get(MEDIA_FILES_KEY).then(res => {
+      this.mediaFiles = JSON.parse(res) || [];
+    });
+  }
+
+  captureAudio() {
+    this.mediaCapture.captureAudio().then(res => {
+      this.storeMediaFiles(res);
+    }, (err: CaptureError) => console.error(err));
+  }
+
+  storeMediaFiles(files) {
+    this.storage.get(MEDIA_FILES_KEY).then(res => {
+      if (res) {
+        let arr = JSON.parse(res);
+        arr = arr.concat(files);
+        this.storage.set(MEDIA_FILES_KEY, JSON.stringify(arr));
+      } else {
+        this.storage.set(MEDIA_FILES_KEY, JSON.stringify(files));
+      }
+      this.mediaFiles = this.mediaFiles.concat(files);
+    });
   }
 }
